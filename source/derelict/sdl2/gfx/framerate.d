c124c7121cdd69f484646dc8ccdd9b32699f3379
J.R. van Scheijen <github@vanscheijen.nl>
2016-03-17 02:39:34 +0100

    Added rotozoom and framerate functions

---
+++ source/derelict/sdl2/gfx/framerate.d
@@ -0,0 +1,32 @@
+﻿module derelict.sdl2.gfx.framerate;
+
+import derelict.sdl2.sdl;
+
+struct FPSmanager
+{
+    int framecount;
+    float rateticks;
+    int baseticks;
+    int lastticks;
+    int rate;
+}
+
+extern(C) @nogc nothrow
+{
+    // Framerate control
+    alias da_SDL_initFramerate = int function(FPSmanager *manager);
+    alias da_SDL_setFramerate = int function(FPSmanager *manager, int rate);
+    alias da_SDL_getFramerate = int function(FPSmanager *manager);
+    alias da_SDL_getFramecount = int function(FPSmanager *manager);
+    alias da_SDL_framerateDelay = int function(FPSmanager *manager);
+}
+
+__gshared
+{
+    // Framerate control
+    da_SDL_initFramerate SDL_initFramerate;
+    da_SDL_setFramerate SDL_setFramerate;
+    da_SDL_getFramerate SDL_getFramerate;
+    da_SDL_getFramecount SDL_getFramecount;
+    da_SDL_framerateDelay SDL_framerateDelay;
+}
---
+++ source/derelict/sdl2/gfx/rotozoom.d
@@ -0,0 +1,31 @@
+﻿module derelict.sdl2.gfx.rotozoom;
+
+import derelict.sdl2.sdl;
+
+extern(C) @nogc nothrow
+{
+    // Framerate control
+    alias da_transformSurfaceY = void function(SDL_Surface *src, SDL_Surface *dst, int cx, int cy, int isin, int icos, int flipx, int flipy);
+    alias da_rotateSurface90Degrees = SDL_Surface* function(SDL_Surface *src, int numClockwiseTurns);
+    alias da_rotozoomSurfaceSizeXY = void function(int width, int height, double angle, double zoomx, double zoomy, int *dstwidth, int *dstheight);
+    alias da_rotozoomSurfaceSize = void function(int width, int height, double angle, double zoom, int *dstwidth, int *dstheight);
+    alias da_rotozoomSurface = SDL_Surface* function(SDL_Surface *src, double angle, double zoom, int smooth);
+    alias da_rotozoomSurfaceXY = SDL_Surface* function(SDL_Surface *src, double angle, double zoomx, double zoomy, int smooth);
+    alias da_zoomSurfaceSize = void function(int width, int height, double zoomx, double zoomy, int *dstwidth, int *dstheight);
+    alias da_zoomSurface = SDL_Surface* function(SDL_Surface *src, double zoomx, double zoomy, int smooth);
+    alias da_shrinkSurface = SDL_Surface* function(SDL_Surface *src, int factorx, int factory);
+}
+
+__gshared
+{
+    // Framerate control
+    da_transformSurfaceY transformSurfaceY;
+    da_rotateSurface90Degrees rotateSurface90Degrees;
+    da_rotozoomSurfaceSizeXY rotozoomSurfaceSizeXY;
+    da_rotozoomSurfaceSize rotozoomSurfaceSize;
+    da_rotozoomSurface rotozoomSurface;
+    da_rotozoomSurfaceXY rotozoomSurfaceXY;
+    da_zoomSurfaceSize zoomSurfaceSize;
+    da_zoomSurface zoomSurface;
+    da_shrinkSurface shrinkSurface;
+}
